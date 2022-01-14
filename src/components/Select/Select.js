3f88986bc19a0027eb5c8a1b219e6930b85def67
Marcelo Risoli <celorisoli@gmail.com>
2022-01-14 17:45:03 -0300

    part 3 shell

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -32,7 +32,10 @@ const Select = ({ label, value, children, ...delegated }) => {
   );
 };
 
-const Wrapper = styled.label``;
+const Wrapper = styled.label`
+  align-items; baseline;
+  display: flex;
+`;
 
 const VisibleLabel = styled.span`
   color: ${COLORS.gray[700]};
